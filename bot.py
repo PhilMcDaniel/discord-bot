d92891a647fe445453c731948a4487ff97a732d7
Phil McDaniel <mcdanipc@gmail.com>
2020-12-24 20:25:45 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import config
 
+
 #https://discord.com/developers/applications
 #https://discordpy.readthedocs.io/en/latest/api.html
 
@@ -9,6 +10,11 @@ TOKEN = config.DISCORD_TOKEN
 
 client = discord.Client()
 
+# login
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
 
 @client.event
 async def on_message(message):
@@ -16,12 +22,14 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-
+# simple if statements looking for specific text in messages
     if message.content == '!movies':
         await message.channel.send(config.MOVIES_LINK)
     if message.content == '!erotica':
         await message.channel.send(f'Here you go, {message.author} - https://www.reddit.com/r/Erotica/')
     if 'happy birthday' in message.content.lower():
         await message.channel.send('Happy Birthday! 🎈🎉')
+    if message.content == '!shrug':
+        await message.channel.send('¯\_(ツ)_/¯')
 client.run(TOKEN)
 #client.close()
