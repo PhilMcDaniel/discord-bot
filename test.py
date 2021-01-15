2d40f14f43a5ed8ce8942e2bcff6abdb1b1681e6
Phil McDaniel <mcdanipc@gmail.com>
2021-01-14 19:35:38 -0600

    update for uptime file prep
    test scripts for uptime file. will be added to bot.py with next commit

---
+++ test.py
@@ -0,0 +1,27 @@
+import datetime
+import time
+import atexit
+from decimal import *
+
+getcontext().prec = 15
+start = datetime.datetime.now()
+time.sleep(5)
+
+delta = (datetime.datetime.now()-start).total_seconds()
+print(delta)
+
+#read old total
+with open("uptime.txt",'r') as file:
+    olduptime = file.readline()
+    print(olduptime)
+
+#add old total to current uptime
+newuptime = round(Decimal(delta)) + round(Decimal(olduptime))
+
+#write new uptime to file
+def updateuptime():
+    with open("uptime.txt", "w") as outfile:
+        #print(str(newuptime))
+        outfile.write(str(newuptime))
+        #print(datetime.timedelta(seconds=newuptime))
+atexit.register(updateuptime)
