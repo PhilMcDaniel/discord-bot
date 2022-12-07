ff3ee022bea600279cd5ddebd38e550fd0ec4522
Phil McDaniel <mcdanipc@gmail.com>
2022-12-07 10:05:16 -0600

    update test script with image edit & variation

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
 import openai
 import config
+import os
+
+print(os.getcwd())
 
 
 openai.api_key = config.API_KEY
 
-try:
-    response = openai.Image.create(
-    prompt="testing bar strength trump",
-    n=1,
-    size="512x512")
 
-    response['data'][0]['url']
+response = openai.Image.create(
+prompt="a close up, studio photographic portrait of a white siamese cat that looks curious, backlit ears",
+n=1,
+size="512x512")
 
-except openai.InvalidRequestError:
-    print("Your prompt contained text that was not allowed by the openai safety system.")
+response = openai.Image.create_edit(
+image=open("images\\otter2.PNG", "rb"),
+mask=open("images\\otter2_mask.PNG", "rb"),
+prompt="otter in yellow and blue water",
+n=1,
+size="512x512"
+)
+
+response = openai.Image.create_variation(
+image=open("images\\shelby2.PNG", "rb"),
+n=1,
+size="512x512"
+)
+
+response['data'][0]['url']
