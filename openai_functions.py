6400f8a21125b8eb4f718c21c25eef01edcaa3db
Phil McDaniel <mcdanipc@gmail.com>
2024-11-29 17:15:30 -0600

    Update openai call behind !aitext

--- openai_functions.py
+++ openai_functions.py
@@ -1,30 +1,30 @@
-import openai
+from openai import OpenAI
 import os
 import config
 import requests
 
 
 #authenticate openai
-openai.api_key = config.API_KEY
-
+client = OpenAI(api_key=config.API_KEY)
 
 def get_aitext_completion(aitext_text_prompt):
     """retrives ai text from openai completion ai"""
     try:
-        response = openai.Completion.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=aitext_text_prompt,
-        max_tokens=1024,
-        n=1,
-        stream=False
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                    {"role": "user", "content": f"{aitext_text_prompt}"}
+                    ],
+            max_tokens=2048,
+            n=1
         )
-        return response['choices'][0]['text']
+        return response.choices[0].message.content
     except Exception as err:
         #return print(f"Unexpected {err=}, {type(err)=}")
         raise
     except:
         pass
-#get_aitext_completion('What python 3 code is needed to read an image from a URL using the requests library?')
+#get_aitext_completion('Tell me a short moral story in a haiku')
 
 
 #print(get_aiart_variation('https://cdn.britannica.com/35/7535-004-99D14F9B/Winston-Churchill-Yousuf-Karsh-1941.jpg')[0]['url'])
